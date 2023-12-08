d844481405b8f2d6ba53c5dbfeb45c0ff17dfbbb
KushigeRen <xs_caster@yahoo.co.jp>
2023-12-08 21:49:00 +0900

    fix:予定の新規作成・編集で、開始日、終了日またはその両方を未入力の状態で保存を押した時にエラーが出る不具合を修正 fix:ルートへのルーティングを追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,12 +1,17 @@
 class Schedule < ApplicationRecord
     validates :title, presence: true, length: {maximum: 20}
-    validates :start_day, presence: true
-    validates :end_day, presence: true
     validates :memo,length:{maximum: 500}
     validate :inconsistent_date
 
     def inconsistent_date
-        errors.add(:end_day, "は開始日より前の日付は登録できません。") unless
-        self.start_day < self.end_day
+        if start_day.blank? && end_day.blank?
+            errors.add(:start_day,'と終了日を入力してください')
+        elsif start_day.blank?
+            errors.add(:start_day,'を入力してください')
+        elsif end_day.blank?
+            errors.add(:end_day,'を入力してください')
+        elsif self.start_day > self.end_day
+            errors.add(:end_day,'は開始日より前の日付は登録できません')
+        end
     end
 end
